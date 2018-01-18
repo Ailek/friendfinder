0c5173cc0d494beb0bd5cd7228aceac4fec00963
Ailek <31447124+Ailek@users.noreply.github.com>
2018-01-18 15:13:34 -0500

    last

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,9 +1,59 @@
 //array that holds all the 'friends' that have taken the survey
-//initialize starting with 'dummy' data
+
 var friendList = [
     {
-      name: 'Stefanie Ding',
-      profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
+      name: 'sara',
+      profilePic: 'https://github.com/sarahav2',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+    {
+      name: 'tamara',
+      profilePic: 'https://github.com/Highlyne',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+
+    {
+      name: 'brian',
+      profilePic: 'https://github.com/BrianElliston',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+
+ {
+      name: 'ruha',
+      profilePic: 'https://github.com/Ruha-RP',
       scores: [
         "4",
         "5",
